ea2ecd6f35225b652790372f1b1c6e38ce25e1c4
SuperOmegaCow <SuperOmegaCow@yahoo.com>
2015-08-25 17:53:23 -0400

    This might work.

--- include/Cube.h
+++ include/Cube.h
@@ -16,6 +16,24 @@ class Cube
 
     private:
         LED *leds[8][8][8];
+
+        const int dataPin1 = A0;
+        const int latchPin1 = A1;
+        const int clockPin1 = A2;
+        const int dataPin2 = 11;
+        const int latchPin2 = 12;
+        const int clockPin2 = 13;
+        const int dataPin3 = 2;
+        const int latchPin3 = 3;
+        const int clockPin3 = 4;
+        const int dataPin4 = 5;
+        const int latchPin4 = 6;
+        const int clockPin4 = 7;
+        //CATHODE SHIFT REGISTER PIN DEFINES
+        const int dataPinN = A3;
+        const int latchPinN = A4;
+        const int clockPinN = A5;
+
 };
 
 #endif
--- src/Cube.cpp
+++ src/Cube.cpp
@@ -2,11 +2,28 @@
 
 Cube::Cube()
 {
-    for(int x = 0; x < 8; x++) {
-        for(int y = 0; y < 8; y++) {
-            for(int z = 0; z < 8; z++)
+
+    pinMode(dataPin0, OUTPUT);
+    pinMode(latchPin0, OUTPUT);
+    pinMode(clockPin0, OUTPUT);
+    pinMode(dataPin1, OUTPUT);
+    pinMode(latchPin1, OUTPUT);
+    pinMode(clockPin1, OUTPUT);
+    pinMode(dataPin2, OUTPUT);
+    pinMode(latchPin2, OUTPUT);
+    pinMode(clockPin2, OUTPUT);
+    pinMode(dataPin3, OUTPUT);
+    pinMode(latchPin3, OUTPUT);
+    pinMode(clockPin3, OUTPUT);
+    pinMode(dataPinN, OUTPUT);
+    pinMode(latchPinN, OUTPUT);
+    pinMode(clockPinN, OUTPUT);
+
+    for(int x = 1; x <= 8; x++) {
+        for(int y = 1; y <= 8; y++) {
+            for(int z = 1; z <= 8; z++)
             {
-                leds[x][y][y] = new LED(x + 1, y + 1, z + 1);
+                leds[x][y][y] = new LED(x, y, z);
             }
         }
     }
@@ -19,7 +36,80 @@ LED Cube::getAt(int x, int y, int z)
 
 void Cube::update()
 {
+    for(int z = 2; z <= 8; z += 2)
+    {
+        int xAdd = 0;
+        for(int x = 1; x <= 8; x++)
+        {
+            int yAdd = 0;
+            for(int y = 1; y <= 8; y++) {
+                LED led = leds[x][y][z];
+                if(led.on)
+                {
+                    xAdd += 2^x;
+                    yAdd += 2^y;
+                }
+            }
+        }
+        digitalWrite(latchPin0, LOW);
+        digitalWrite(latchPin1, LOW);
+        digitalWrite(latchPin2, LOW);
+        digitalWrite(latchPin3, LOW);
+        if(z == 1)
+            {
+            shiftOut(dataPin0, clockPin0, MSBFIRST, xAdd);
+        } else if(z == 3)
+        {
+            shiftOut(dataPin1, clockPin1, MSBFIRST, xAdd);
+        } else if(z == 5)
+        {
+            shiftOut(dataPin2, clockPin2, MSBFIRST, xAdd);
+        } else if(z == 7)
+        {
+            shiftOut(dataPin3, clockPin3, MSBFIRST, xAdd);
+        }
+        digitalWrite(latchPinN, LOW);
+        shiftOut(dataPinN, clockPinN, MSBFIRST, yAdd);
+        digitalWrite(latchPinN, HIGH);
 
+    }
+
+    for(int z = 1; z <= 7; z += 2)
+    {
+        int xAdd = 0;
+        for(int x = 1; x <= 8; x++)
+        {
+            int yAdd = 0;
+            for(int y = 1; y <= 8; y++) {
+                LED led = leds[x][y][z];
+                if(led.on)
+                {
+                    xAdd += 2^x;
+                    yAdd += 2^y;
+                }
+            }
+        }
+        if(z == 1) {
+            shiftOut(dataPin0, clockPin0, MSBFIRST, xAdd);
+        } else if(z == 3)
+        {
+            shiftOut(dataPin1, clockPin1, MSBFIRST, xAdd);
+        } else if(z == 5)
+        {
+            shiftOut(dataPin2, clockPin2, MSBFIRST, xAdd);
+        } else if(z == 7)
+        {
+            shiftOut(dataPin3, clockPin3, MSBFIRST, xAdd);
+        }
+        //Write verticle
+        digitalWrite(latchPin0, HIGH);
+        digitalWrite(latchPin1, HIGH);
+        digitalWrite(latchPin2, HIGH);
+        digitalWrite(latchPin3, HIGH);
+        digitalWrite(latchPinN, LOW);
+        shiftOut(dataPinN, clockPinN, MSBFIRST, yAdd);
+        digitalWrite(latchPinN, HIGH);
+    }
 }
 
 Cube::~Cube()
